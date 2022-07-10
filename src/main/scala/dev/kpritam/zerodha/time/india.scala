69645f548cef925e47d8cca5cef844c773b832a8
Pritam Kadam <pkadam@investvoyager.com>
2022-07-10 19:14:45 +0530

    fix nextWeekday logic

--- src/main/scala/dev/kpritam/zerodha/time/india.scala
+++ src/main/scala/dev/kpritam/zerodha/time/india.scala
@@ -3,5 +3,6 @@ package dev.kpritam.zerodha.time
 import java.time.ZoneId
 import java.util.Date
 
-extension (date: Date)
-  def toIndiaLocalDate = date.toInstant.atZone(ZoneId.of("Asia/Kolkata")).toLocalDate
+val indiaZone = ZoneId.of("Asia/Kolkata")
+
+extension (date: Date) def toIndiaLocalDate = date.toInstant.atZone(indiaZone).toLocalDate
--- src/main/scala/dev/kpritam/zerodha/time/weekday.scala
+++ src/main/scala/dev/kpritam/zerodha/time/weekday.scala
@@ -1,10 +1,13 @@
 package dev.kpritam.zerodha.time
 
-import java.time.LocalDate
+import java.time.{LocalDate, ZoneId}
 import java.util.Calendar
 
 def nextWeekday(nextDayOfWeek: Int): LocalDate =
-  val cal = Calendar.getInstance()
+  val cal      = Calendar.getInstance()
   cal.set(Calendar.DAY_OF_WEEK, nextDayOfWeek)
+  val curWeek  = cal.getTime.toIndiaLocalDate
   cal.add(Calendar.WEEK_OF_YEAR, 1)
-  cal.getTime.toIndiaLocalDate
+  val nextWeek = cal.getTime.toIndiaLocalDate
+  val now      = LocalDate.now(indiaZone)
+  if now.isBefore(curWeek) then curWeek else nextWeek
