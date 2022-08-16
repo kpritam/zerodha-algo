9ea653dc1720361c217daacda1588a80459a1b29
Pritam Kadam <pkadam@investvoyager.com>
2022-08-16 20:21:55 +0530

    use 24 hour clock

--- strategies/src/main/scala/dev/kpritam/zerodha/seed.scala
+++ strategies/src/main/scala/dev/kpritam/zerodha/seed.scala
@@ -7,14 +7,20 @@ import dev.kpritam.zerodha.kite.time.indiaZone
 import dev.kpritam.zerodha.time.nextWeekday
 import zio.*
 
-import java.time.LocalDateTime
+import java.time.{LocalDate, LocalDateTime}
 import java.util.Calendar
 
 private val expiryDate = nextWeekday(Calendar.THURSDAY)
 
+case class InstrumentsNotFound(expiryDate: LocalDate) extends Throwable {
+  override def getMessage: String = s"Instruments not found for expiry date of: $expiryDate"
+}
+
 def seedInstrumentsIfNeeded =
   for
     _ <- ZIO.logDebug(s"Downloading instruments expiring at $expiryDate...")
-    r <- KiteService.seedAllInstruments(expiryDate)
+    r <- KiteService
+           .seedAllInstruments(expiryDate)
+           .filterOrDie(_.nonEmpty)(InstrumentsNotFound(expiryDate))
     _ <- ZIO.logDebug(s"Total: ${r.size} instruments downloaded and stored in DB")
   yield ()
--- strategies/src/main/scala/dev/kpritam/zerodha/strategies/everyday/run.scala
+++ strategies/src/main/scala/dev/kpritam/zerodha/strategies/everyday/run.scala
@@ -15,11 +15,11 @@ val run =
             .fork
     f2 <- EverydayStrategy.modifyPendingOrders
             .catchAndLog("[1:30] Modify failed")
-            .schedule(onceDay(1, 30))
+            .schedule(onceDay(13, 30))
             .fork
     f3 <- EverydayStrategy.modifyPendingOrders
             .catchAndLog("[2:30] Modify failed")
-            .schedule(onceDay(2, 30))
+            .schedule(onceDay(14, 30))
             .fork
     _  <- f1.zip(f2).zip(f3).await
   yield ()
